a1bb54a5b44406a2516e3b7e9747e781e770dfce
Jesse House <mail@jessehouse.com>
2014-12-09 11:29:03 -0800

    Bump version to 0.6.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mail@jessehouse.com'
 license          'The MIT License (MIT)'
 description      'Enable postgresql WAL archiving to S3 with wal-e'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.0'
+version          '0.6.0'
 
 supports "ubuntu"
 supports "debian"
